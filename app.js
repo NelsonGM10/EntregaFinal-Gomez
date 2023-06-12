0d7e9ed6225985d8c003e733ba8e8d7a4a189647
Nelson Gómez <n.gm10@hotmail.com>
2023-06-12 18:44:17 -0500

    PreEntrega3-Gomez

--- app.js
+++ app.js
@@ -1,64 +1,53 @@
- function mensajeBienvenida (){
-    alert("Bienvenid@ a DrinkHouse")
-    let nombreCliente = prompt("Ingrese su nombre")
-    let apellidoCliente = prompt("Ingrese su apellido")
-    alert("Bienvenid@" + " " + nombreCliente + " " + apellidoCliente )
-   }
-  mensajeBienvenida()
-
-  function validarCliente(){
-  let contrasenia = "hola"
-  let ingresarClave = prompt ("ingrese su contraseña")
-    if (ingresarClave == "hola" || ingresarClave =="HOLA") {
-    alert("Acceso permitido")
-   // cerveza()
-   }else{
-     alert("Acceso denegado")
-     }
-    
-   }
-  validarCliente()
-
-function cerveza ( nombre, precio, cantidad){
-    this.nombre = nombre = prompt(`¿Que cerveza deseas tomar hoy?
-    Corona
-    Heineken
-    Club Colombia
-    Smirnoff`)
-    this.precio = precio
-    this.cantidad = cantidad = prompt ("¿Cuantas se te antojan hoy?")
-    this.compra = function(){
- alert ("El total de tu compra es " + this.precio * this.cantidad + " por " + this.cantidad + " botellas de " + this.nombre)
-    
-    }
-}
-
-const pedidoUno = new cerveza ("", /*valor por unidad*/ 9000, 3)
-
-        
-pedidoUno.compra()
-    
-const carrito = []
-
-carrito.push(pedidoUno)
-console.log(carrito)
-
-
-   let totalCompra = carrito.reduce((accum , prod, cantidad)=>{
-     return accum + prod.precio * prod.cantidad
-  },0)
-console.log("generando factura por " + totalCompra)
-
-const lugar =[
-    {barrio : "La Esperanza"},
-    {barrio: "Rodeo alto"},
-    {barrio: "Santa fe"},
-    {barrio: "Barrio antioquia"}
+const cuadros = [
+{id:goku, precio:70, descripcion:"Goku SSJ4"},
+{id:vegeta, precio:65, descripcion: "Majin Vegeta SSJ"},
+{id:gohan, precio:65, descripcion:"Gohan SSJ2"},
+{id:uzumaki, precio:60, descripcion:"Naruto Uzumaki"},
+{id:sasuke, precio:60, descripcion:"Sasuke Uchiha"},
+{id:madara, precio:60, descripcion:"Madara Uchiha"},
+{id:luffy, precio:70, descripcion:"Monkey D Luffy Gear 5"},
+{id:zoro, precio:65, descripcion:"Roronoa Zoro"},
+{id:asce , precio:60, descripcion:"Portgas D Asce"},
+{id:tanjiro, precio:65, descripcion:"Tanjiro Kamado"},
+{id:zenitsu, precio:60, descripcion:"Zentisu Agatsuma"},
+{id:rengoku, precio:65, descripcion:"Rengoku Kyojuro"},
 ]
 
-let busqueda = lugar.filter((lugar)=> lugar.barrio != "Santa fe")
-console.log(busqueda)
+// let carrito =[];
 
-let sede = lugar.find((lugar)=> lugar.barrio == "rodeo alto")
-//console.log(sede)
+// cuadros.forEach((producto)=>{
+//   add.addEventListener("click",()=>{
+//     compra.push({ id: precio.descripcion
+//     });
+//     //console.log(carrito)
+//   })
+// })
 
+// datos del cliente // 
+const datos = document.querySelector("#conteneForm");
+const nameUser = document.querySelector("#validationTooltip01");
+nameUser.addEventListener("input", (e) => {
+  if (e.target.value === "") {
+    alert("ingrese nombre");
+  }
+});
+const datoCiudad = document.querySelector("#validationTooltip02");
+datoCiudad.addEventListener("input", (e) => {
+  if (e.target.value === "") {
+    alert("ingrese ciudad");
+  }
+});
+const datoDireccion = document.querySelector("#validationTooltip03");
+datoDireccion.addEventListener("input", (e) => {
+  if (e.target.value === "") {
+    alert("ingrese una dirección");
+  }
+});
+datos.addEventListener("submit", confirmarDatos);
+
+function confirmarDatos(e) {
+  e.preventDefault();
+  alert(
+    `Gracias ${nameUser.value} por tu compra, tu pedido será enviado a la ciudad de ${datoCiudad.value} en la direccion  ${datoDireccion.value} `
+  );
+}
