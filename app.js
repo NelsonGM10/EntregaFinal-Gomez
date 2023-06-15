eac37703a906aea0febb97a43043dbc365fb5ef4
Nelson Gómez <n.gm10@hotmail.com>
2023-06-14 22:58:04 -0500

    tercer PreEntrega

--- app.js
+++ app.js
@@ -1,53 +1,105 @@
-const cuadros = [
-{id:goku, precio:70, descripcion:"Goku SSJ4"},
-{id:vegeta, precio:65, descripcion: "Majin Vegeta SSJ"},
-{id:gohan, precio:65, descripcion:"Gohan SSJ2"},
-{id:uzumaki, precio:60, descripcion:"Naruto Uzumaki"},
-{id:sasuke, precio:60, descripcion:"Sasuke Uchiha"},
-{id:madara, precio:60, descripcion:"Madara Uchiha"},
-{id:luffy, precio:70, descripcion:"Monkey D Luffy Gear 5"},
-{id:zoro, precio:65, descripcion:"Roronoa Zoro"},
-{id:asce , precio:60, descripcion:"Portgas D Asce"},
-{id:tanjiro, precio:65, descripcion:"Tanjiro Kamado"},
-{id:zenitsu, precio:60, descripcion:"Zentisu Agatsuma"},
-{id:rengoku, precio:65, descripcion:"Rengoku Kyojuro"},
-]
+const btnCart = document.querySelector(".container-cart-icon");
+const containerCartProducts = document.querySelector(
+  ".container-cart-products"
+);
 
-// let carrito =[];
+btnCart.addEventListener("click", () => {
+  containerCartProducts.classList.toggle("hidden-cart");
+});
+/*----------------------------------------------*/
+const cartInfo = document.querySelector(".cart-product");
+const rowProdcut = document.querySelector(".row-product");
 
-// cuadros.forEach((producto)=>{
-//   add.addEventListener("click",()=>{
-//     compra.push({ id: precio.descripcion
-//     });
-//     //console.log(carrito)
-//   })
-// })
+/* lista contenedor productos*/
+const producsList = document.querySelector(".container-items");
 
-// datos del cliente // 
-const datos = document.querySelector("#conteneForm");
-const nameUser = document.querySelector("#validationTooltip01");
-nameUser.addEventListener("input", (e) => {
-  if (e.target.value === "") {
-    alert("ingrese nombre");
+let allProducts = [];
+
+const valorTotal = document.querySelector(".total-pagar");
+const countProducts = document.querySelector("#contador-productos");
+
+producsList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-add-cart")) {
+    const product = e.target.parentElement;
+    const infoProduct = {
+      quantity: 1,
+      title: product.querySelector("h2").textContent,
+      price: product.querySelector("p").textContent,
+    };
+
+    const exits = allProducts.some(
+      (product) => product.title === infoProduct.title
+    );
+    
+
+    if (exits) {
+      const products = allProducts.map((product) => {
+        if (product.title === infoProduct.title) {
+          product.quantity++;
+          return product;
+        } else {
+          return product;
+        }
+      });
+      allProducts = [...products];
+    } else {
+      allProducts = [...allProducts, infoProduct];
+    }
+    showShop();
   }
 });
-const datoCiudad = document.querySelector("#validationTooltip02");
-datoCiudad.addEventListener("input", (e) => {
-  if (e.target.value === "") {
-    alert("ingrese ciudad");
-  }
-});
-const datoDireccion = document.querySelector("#validationTooltip03");
-datoDireccion.addEventListener("input", (e) => {
-  if (e.target.value === "") {
-    alert("ingrese una dirección");
-  }
-});
-datos.addEventListener("submit", confirmarDatos);
 
-function confirmarDatos(e) {
-  e.preventDefault();
-  alert(
-    `Gracias ${nameUser.value} por tu compra, tu pedido será enviado a la ciudad de ${datoCiudad.value} en la direccion  ${datoDireccion.value} `
-  );
-}
+rowProdcut.addEventListener("click", (e) => {
+  if (e.target.classList.contains("icon-close")) {
+    const product = e.target.parentElement;
+    const title = product.querySelector("p").textContent;
+
+    allProducts = allProducts.filter((product) => product.title !== title);
+    showShop();
+  }
+});
+
+/*limpiar carrito*/
+const showShop = () => {
+  rowProdcut.innerHTML = "";
+
+  let total = 0;
+  let totalShop = 0;
+
+  allProducts.forEach((product) => {
+    const containerProduct = document.createElement("div");
+    containerProduct.classList.add("cart-product");
+
+    containerProduct.innerHTML = `
+        <div class="info-cart-product">
+					  <span class="cantidad-producto-carrito">${product.quantity}</span>
+					  <p class="titulo-producto-carrito">${product.title}</p>
+					  <span class="precio-producto-carrito">${product.price}</span>
+					</div>
+					<svg
+					  xmlns="http://www.w3.org/2000/svg"
+					  fill="none"
+					  viewBox="0 0 24 24"
+					  stroke-width="1.5"
+					  stroke="currentColor"
+					  class="icon-close"
+					>
+					  <path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M6 18L18 6M6 6l12 12"
+					  />
+					</svg>
+        
+        `;
+    rowProdcut.append(containerProduct);
+
+    total = total + parseInt(product.quantity * product.price.slice(1));
+    totalShop = totalShop + product.quantity;
+  });
+
+  valorTotal.innerText = `$${total}`;
+  countProducts.innerText = totalShop;
+};
+
+
